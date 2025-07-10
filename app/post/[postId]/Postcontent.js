40f4fe32191660fb4c2e6fd8701bd8dbd743036b
ANR-DEVl <anouarkira888@gmail.com>
2025-07-10 16:26:12 +0100

    shozie-fifth

--- app/post/[postId]/Postcontent.js
+++ app/post/[postId]/Postcontent.js
@@ -19,7 +19,8 @@ import styles from "../mainproduct.module.css"
 export default function Postcontent({postId,products}) {
 
 
-    const product = products.find(product => product.id === postId);
+
+    const product = products.find(product => product.id == postId);
 // console.log('title:', product.title, 'type:', typeof product.title);
 // console.log('shortdisc:', product.shortdisc, 'type:', typeof product.shortdisc);
 // console.log('rate:', product.rate, 'type:', typeof product.rate);
